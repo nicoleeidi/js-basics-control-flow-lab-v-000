d75a4f0161570befe863ed6c58e9c3252f3aa613
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-15 02:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // Write your code in this file!
 scuberGreetingForFeet(feet){
   if feet <= 400
-  return "This one is on me!"
+    return "This one is on me!"
   elsif feet > 2000
-  return "I will gladly take your thirty bucks."
+    return "I will gladly take your thirty bucks."
   elsif feet > 2500
-  return "No can do."
+    return "No can do."
+  end 
 
 
 }
-ternaryCheckCity(){
-  
+ternaryCheckCity(city){
+  if city === "NYC"
+    return "Ok, sounds good."
+  else 
+    return "No go."
+  end 
 }
