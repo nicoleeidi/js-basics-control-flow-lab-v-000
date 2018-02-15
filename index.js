e02d847b6dc7427fef9c726bfabc2437f3425399
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-15 02:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code in this file!
 scuberGreetingForFeet(feet){
-  if feet < 400 
+  if feet <= 400
   return "This one is on me!"
-  else 
+  elsif feet > 2000
+  return "I will gladly take your thirty bucks."
+  elsif feet > 2500
+  return "No can do."
   
+
 }
