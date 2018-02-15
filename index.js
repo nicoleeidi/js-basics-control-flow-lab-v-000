24e1573b7b80d64474ca8026213efd13ff8e0f0f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-15 03:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // Write your code in this file!
 let scuberGreetingForFeet
-  if feet <= 400
+  if (feet <= 400){
     scuberGreetingForFeet= "This one is on me!"
-  else if feet > 2000
+  }
+    
+  else if (feet > 2000) {
     scuberGreetingForFeet= "I will gladly take your thirty bucks."
-  else if feet > 2500
+  }
+    
+  else if (feet > 2500) {
     scuberGreetingForFeet= "No can do."
-  end
+  }
+    
+  
 
 
 }
 let ternaryCheckCity;
-  if city === "NYC"
+  if (city === "NYC"){
     ternaryCheckCity= "Ok, sounds good."
-  else
+  }
+    
+  else{
     ternaryCheckCity= "No go."
-  end
-}
+  }
+
 let switchOnCharmFromTip
   switch (tip) {
     case "generous":
@@ -29,6 +37,3 @@ let switchOnCharmFromTip
     switchOnCharmFromTip= "Bye."
     break;
   }
-
-
-
